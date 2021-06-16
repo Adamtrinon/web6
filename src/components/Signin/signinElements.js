12cabec450dad8f9e0e10c7af5b436c238d0553a
Adamtrinon <adamTrinon@icloud.com>
2021-06-15 21:15:34 -0700

    message

--- src/components/Signin/signinElements.js
+++ src/components/Signin/signinElements.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components'
 import {Link} from 'react-router-dom'
-import { FaCentercode } from 'react-icons/fa'
 
 export const Container = styled.div`
     min-height:692px;
